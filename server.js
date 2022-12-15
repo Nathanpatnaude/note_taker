deb722966867aaf029337d6abb12b967802900d0
Nathan Patnaude <nathanpatnaude@gmail.com>
2022-12-15 12:48:01 -0500

    added a path for api/notes

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const util = require('util');
 const express = require('express');
 const path = require('path');
+const readFromFile = util.promisify(fs.readFile);
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,10 +12,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
-// app.get('/', (req, res) =>
-// res.sendFile(path.join(__dirname, ''))
-// );
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) =>
+  res.json(JSON.parse(data))
+  );
+});
 
 app.get('*', (req, res) =>
 res.sendFile(path.join(__dirname, 'public/index.html'))
